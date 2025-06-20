6d7b3f3e2fdfbd14f41b9cdbc75a56c7d8ca0822
David Rivero <davidmiguel22573@gmail.com>
2025-06-20 03:42:48 -0700

    feature: add argon2id hashing for password

---
+++ environment.go
@@ -0,0 +1,50 @@
+package main
+
+import "maps"
+
+type VariableMap map[string]string
+type Environment struct {
+	Variables VariableMap `json:"variables"`
+}
+
+func NewEnvironment() *Environment {
+	return &Environment{
+		Variables: make(VariableMap),
+	}
+}
+func (e *Environment) SetVariable(name, value string) {
+	e.Variables[name] = value
+}
+func (e *Environment) GetVariable(name string) (string, bool) {
+	value, exists := e.Variables[name]
+	return value, exists
+}
+
+func (e *Environment) DeleteVariable(name string) {
+	delete(e.Variables, name)
+}
+
+func (e *Environment) Clear() {
+	e.Variables = make(VariableMap)
+}
+func (e *Environment) Clone() *Environment {
+	clone := NewEnvironment()
+	clone.Variables = make(VariableMap)
+	clone.Variables = maps.Clone(clone.Variables)
+	return clone
+}
+
+func (e *Environment) Merge(other *Environment) {
+	if other == nil {
+		return
+	}
+	maps.Copy(e.Variables, other.Variables)
+}
+func (e *Environment) MergeWith(other *Environment) *Environment {
+	if other == nil {
+		return e.Clone()
+	}
+	clone := e.Clone()
+	clone.Merge(other)
+	return clone
+}
---
+++ workspace.go
@@ -0,0 +1,12 @@
+package main
+
+type Workspace struct {
+	Name        string       `json:"name"`
+	Environment *Environment `json:"environment,omitempty"`
+}
+
+func NewWorkspace(name string) *Workspace {
+	return &Workspace{
+		Name: name,
+	}
+}
